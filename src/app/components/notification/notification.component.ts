d874bad6d3e890094ad220d1707f38f8f1f86369
Pomazan Vladislav <pomazanvi@gmail.com>
2018-04-22 13:02:04 +0300

    update

--- src/app/components/notification/notification.component.ts
+++ src/app/components/notification/notification.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {style, state, animate, transition, trigger} from '@angular/animations';
 import {NotifyService} from '../../services/notify.service';
 
 @Component({
@@ -6,6 +7,18 @@ import {NotifyService} from '../../services/notify.service';
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.scss'],
   encapsulation: ViewEncapsulation.None,
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [   // :enter is alias to 'void => *'
+        style({opacity: 0}),
+        animate(1000, style({opacity: 1}))
+      ]),
+      transition(':leave', [   // :leave is alias to '* => void'
+        animate(1000, style({opacity: 0}))
+      ])
+    ])
+  ]
+
 })
 export class NotificationComponent implements OnInit {
 
